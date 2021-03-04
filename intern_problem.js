79165e59d56c714b1299a9f9e31c74f7a8af7708
Nancy Ng <nanc.ng1@gmail.com>
2021-03-04 15:52:01 -0500

    made changes to js file

--- intern_problem.js
+++ intern_problem.js
@@ -1,7 +1,7 @@
 
 var request = require('request');
 var username = 'apiuser';
-var password = 'APIKEY HERE';
+var password = 'APIKEY';
 var options = {
     url: 'https://api.myngp.com/v2/broadcastEmails',
     auth: {
@@ -24,13 +24,15 @@ request(options, function (err, res, body) {
     //getting items from json file
     var items_list = new_dictionary["items"]
 
+
+
     //iterating through the item list to get the id and the name of the email
     for (var i = 0; i < items_list.length; i++) {
         console.log(items_list[i]["emailMessageId"] + " " + items_list[i]["name"]);
     }
 
+
     //prints out total count of emails
     console.log("Total: " + new_dictionary["count"])
 });
 
-// console.log(items_)
